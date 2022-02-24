20b1db3ec831a3d5e3b113e63b15c00ed527578e
Jeff Workman <jworkhollr@aol.com>
2022-02-24 13:41:31 -0500

    Add project files.

---
+++ SalesMvc/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesMvc.Models {
+    public class Order {
+
+        public int Id { get; set; }
+        [Required, StringLength(30)]
+        public string Desription { get; set; }
+        [Column(TypeName = "decimal(11, 2)")]
+        public decimal Total { get; set; }
+
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+}
